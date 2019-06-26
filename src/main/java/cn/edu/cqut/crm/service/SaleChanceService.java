75168c3cd611cbf5f036bb31b8b1333bbba07d9b
coolzhang666 <2227603597@qq.com>
2019-06-26 23:11:03 +0800

    完成一部分客户流失预警功能

--- src/main/java/cn/edu/cqut/crm/service/SaleChanceService.java
+++ src/main/java/cn/edu/cqut/crm/service/SaleChanceService.java
@@ -4,13 +4,10 @@ import cn.edu.cqut.crm.dao.SaleChanceMapper;
 import cn.edu.cqut.crm.model.Contact;
 import cn.edu.cqut.crm.model.Customer;
 import cn.edu.cqut.crm.model.SaleChance;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.text.DateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -31,14 +28,16 @@ public class SaleChanceService {
         if (saleChance.getChanceStatus().equals("已分配")) {
 //            System.out.println("aaaa");
             saleChance.setDispatchTime(new Date());
-        }else{
+        } else {
             Date date = null;
             saleChance.setDispatchTime(date);
         }
         saleChanceMapper.insert(saleChance);
     }
+
     /**
      * 查询所有的销售机会
+     *
      * @return 返回销售机会列表
      */
     public List<SaleChance> getAllSaleChance() {
