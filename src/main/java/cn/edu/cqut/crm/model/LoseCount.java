328cdd37ca9c78bf71afc0583d9d0fe428469206
BeanWhite <45782207+BeanWhite@users.noreply.github.com>
2019-06-27 16:53:48 +0800

    修复交往记录bug
    修复交往记录bug

---
+++ src/main/java/cn/edu/cqut/crm/model/LoseCount.java
@@ -0,0 +1,38 @@
+package cn.edu.cqut.crm.model;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class LoseCount {
+    
+   
+    @JSONField(name = "totalNum")
+    private int totalNum;
+    
+    @JSONField(name = "type")
+    private String type;
+
+    public int getTotalNum() {
+        return totalNum;
+    }
+
+    public void setTotalNum(int totalNum) {
+        this.totalNum = totalNum;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "LoseCount{" +
+                "totalNum=" + totalNum +
+                ", type='" + type + '\'' +
+                '}';
+    }
+}
