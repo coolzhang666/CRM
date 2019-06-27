a8d953035243335bc770b04ae2d5c48d647934cd
OuYanglin1 <49508831+OuYanglin1@users.noreply.github.com>
2019-06-27 16:54:43 +0800

    客户贡献分析，对客户下单总金额进行统计
    客户贡献分析，对客户下单总金额进行统计

--- src/main/java/cn/edu/cqut/crm/service/OrderService.java
+++ src/main/java/cn/edu/cqut/crm/service/OrderService.java
@@ -2,6 +2,7 @@ package cn.edu.cqut.crm.service;
 
 import cn.edu.cqut.crm.dao.OrderMapper;
 import cn.edu.cqut.crm.model.Order;
+import cn.edu.cqut.crm.model.OrderStatistics;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +13,15 @@ public class OrderService {
     @Autowired
     private OrderMapper orderMapper;
 
-    public List<Order> getAllOrder() {
+    public List<OrderStatistics> getAllOrder() {
         return orderMapper.selectAll();
     }
+
+    public List<OrderStatistics> getYear16(){ return orderMapper.selectYear16();}
+
+    public List<OrderStatistics> getYear17(){ return orderMapper.selectYear17();}
+
+    public List<OrderStatistics> getYear18(){ return orderMapper.selectYear18();}
+
+    public List<OrderStatistics> getYear19(){ return orderMapper.selectYear19();}
 }
