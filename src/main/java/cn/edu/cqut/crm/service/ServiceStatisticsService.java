0d4e9aa1539ef231ba0d8566c75e08cedadb39eb
litingchuan <1461382632@qq.com>
2019-06-28 09:00:13 +0800

    服务反馈

--- src/main/java/cn/edu/cqut/crm/service/ServiceStatisticsService.java
+++ src/main/java/cn/edu/cqut/crm/service/ServiceStatisticsService.java
@@ -2,6 +2,8 @@ package cn.edu.cqut.crm.service;
 
 import cn.edu.cqut.crm.dao.ServiceStatisticsMapper;
 import cn.edu.cqut.crm.model.ServiceStatistics;
+import cn.edu.cqut.crm.util.ReturnObject;
+import cn.edu.cqut.crm.util.ReturnUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,18 +17,32 @@ public class ServiceStatisticsService {
     @Autowired
     private ServiceStatisticsMapper serviceStatisticsMapper;
 
-    public Map<String,List<String>> getServiceType() {
+
+    public Map<String ,List<String>> getServiceType() {
         List<ServiceStatistics> list = serviceStatisticsMapper.getServiceType();
-        List<String> countlist = new ArrayList<>();
-        List<String> typelist = new ArrayList<>();
-        for(ServiceStatistics service:list) {
-            countlist.add(service.getCountService());
-            typelist.add(service.getServiceType());
+        List<String> list1 = new ArrayList<>();
+        List<String> list2 = new ArrayList<>();
+        for(ServiceStatistics serviceStatistics: list) {
+            list1.add( serviceStatistics.getServiceType());
+            list2.add(serviceStatistics.getCountService());
         }
         Map<String ,List<String>> map = new HashMap<>();
-        map.put("count",countlist);
-        map.put("type",typelist);
+        map.put("type",list1);
+        map.put("count",list2);
+        return map;
+    }
 
+    public Map<String ,List<String>> getDateCount(String create_time) {
+        List<ServiceStatistics> list = serviceStatisticsMapper.getDateCount(create_time);
+        List<String> list1 = new ArrayList<>();
+        List<String> list2 = new ArrayList<>();
+        for(ServiceStatistics serviceStatistics: list) {
+            list1.add( serviceStatistics.getServiceType());
+            list2.add(serviceStatistics.getCountService());
+        }
+        Map<String ,List<String>> map = new HashMap<>();
+        map.put("type",list1);
+        map.put("count",list2);
         return map;
     }
 }
